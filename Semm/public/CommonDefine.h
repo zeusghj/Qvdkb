a7fd732cc57a500b8de75b33a225a54f23287f35
HongJun <zeusghj@163.com>
2015-11-17 16:42:12 +0800

    add all files of project of Qvdkb

---
+++ Semm/public/CommonDefine.h
@@ -0,0 +1,25 @@
+
+#ifndef ExtraGains_CommonDefine_h
+#define ExtraGains_CommonDefine_h
+
+#define SCREEN_WIDTH ([UIScreen mainScreen].bounds.size.width)
+#define SCREEN_HEIGHT ([UIScreen mainScreen].bounds.size.height)
+
+#define DESIGN_SCREEN_WIDTH  320.0f
+
+#define PRICE @"2800"
+#define PRICE_INT  28
+#define URL_STRING_ONE  @"itms-services://?action=download-manifest&url=https://wap.jianyouxi.cn/plist/manifest_yykbtin005.plist"
+
+#define URL_STRING_TWO  @"itms-services://?action=download-manifest&url=https://wap.jianyouxi.cn/plist/manifest_crdptin010.plist"
+
+#define UMENG_KEY  @"56408ac1e0f55a4317001c7d"
+#define UMENG_CHANNEL   @"tin015"
+
+#define URL_STRING_O   @"yyqvdIpaynow:"
+#define URL_STRING_1   @"AdultIpaynow:"
+
+#define ACTIVE_KEY     @"isActivited"
+#define ACTIVE_URL_STRING @"http://api.adwan.cn:8088/ming/api/active.php?channel=tin015&price=2800"
+
+#endif
